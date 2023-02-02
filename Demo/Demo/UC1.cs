56b27965d6ee8a40bc8efe5dc40a849013939117
vijay <vijaydurkikar1@gmail.com>
2023-02-02 09:04:32 +0530

    [vijay]Add:UC1

---
+++ Demo/Demo/UC1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    public class UC1
+    {
+        public static void MyMethod();
+
+    }
+}
